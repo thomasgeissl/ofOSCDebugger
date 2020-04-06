76a685b10e174cd5afb5f002bc249c4061aa2658
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-06 23:42:42 +0200

    installing dependencies

--- scripts/windows/install.sh
+++ scripts/windows/install.sh
@@ -6,6 +6,7 @@ GH_REPO=$(basename $TRAVIS_REPO_SLUG)
 curl -O https://openframeworks.cc/versions/v${OFVERSION}/of_v${OFVERSION}_${OFPLATFORM}_release.zip
 unzip -a -qq of_v${OFVERSION}_${OFPLATFORM}_release.zip
 rm of_v${OFVERSION}_${OFPLATFORM}_release.zip
+of_v${OFVERSION}_${OFPLATFORM}_release/scripts/${OFPLATFORM}/install_dependencies.sh -y
 cd of_v${OFVERSION}_${OFPLATFORM}_release/apps
 mkdir ${GH_USER} && cd $_
 git clone https://github.com/${TRAVIS_REPO_SLUG}.git && cd ${GH_REPO}
