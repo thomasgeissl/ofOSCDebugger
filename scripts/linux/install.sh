32d0a6ca0b4d02e0ebc2185d5f83542fc4b9ec91
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-06 23:07:29 +0200

    improved install scripts

--- scripts/linux/install.sh
+++ scripts/linux/install.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 OFVERSION=0.11.0
+OFPLATFORM=linux64gcc6
 GH_USER=$(dirname $TRAVIS_REPO_SLUG)
 GH_REPO=$(basename $TRAVIS_REPO_SLUG)
 
-curl -O https://openframeworks.cc/versions/v${OFVERSION}/of_v${OFVERSION}_linux64gcc6_release.tar.gz
-tar -zxf of_v${OFVERSION}_linux64gcc6_release.tar.gz
-rm of_v${OFVERSION}_linux64gcc6_release.tar.gz
-sudo of_v${OFVERSION}_linux64gcc6_release/scripts/linux/ubuntu/install_dependencies.sh -y
-cd of_v${OFVERSION}_linux64gcc6_release/apps
-mkdir ${GH_USER} && cd $_
+curl -O https://openframeworks.cc/versions/v${OFVERSION}/of_v${OFVERSION}_${OFPLATFORM}_release.tar.gz
+tar -zxf of_v${OFVERSION}_${OFPLATFORM}_release.tar.gz
+rm of_v${OFVERSION}_${OFPLATFORM}_release.tar.gz
+sudo of_v${OFVERSION}_${OFPLATFORM}_release/scripts/linux/ubuntu/install_dependencies.sh -y
+cd of_v${OFVERSION}_${OFPLATFORM}_release/apps
+mkdir ${GH_USER} && cd ${GH_USER}
 git clone https://github.com/${TRAVIS_REPO_SLUG}.git && cd ${GH_REPO}
 make
--- scripts/osx/install.sh
+++ scripts/osx/install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 OFVERSION=0.11.0
-OFPLATTFORM=osx
+OFPLATFORM=osx
 GH_USER=$(dirname $TRAVIS_REPO_SLUG)
 GH_REPO=$(basename $TRAVIS_REPO_SLUG)
 
-curl -O https://openframeworks.cc/versions/v${OFVERSION}/of_v${OFVERSION}_${OFPLATTFORM}_release.zip
-unzip -a -qq of_v${OFVERSION}_${OFPLATTFORM}_release.zip
-rm of_v${OFVERSION}_${OFPLATTFORM}_release.zip
-cd of_v${OFVERSION}_${OFPLATTFORM}_release/apps
+curl -O https://openframeworks.cc/versions/v${OFVERSION}/of_v${OFVERSION}_${OFPLATFORM}_release.zip
+unzip -a -qq of_v${OFVERSION}_${OFPLATFORM}_release.zip
+rm of_v${OFVERSION}_${OFPLATFORM}_release.zip
+cd of_v${OFVERSION}_${OFPLATFORM}_release/apps
 mkdir ${GH_USER} && cd $_
 git clone https://github.com/${TRAVIS_REPO_SLUG}.git && cd ${GH_REPO}
 make
